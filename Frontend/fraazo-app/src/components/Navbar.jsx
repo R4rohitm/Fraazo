3be6b542592d81cc07b23de7ec88e4400e697fa8
R4rohitm <r4rohitm45@gmail.com>
2022-07-20 14:55:34 +0530

    rr

---
+++ Frontend/fraazo-app/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul class="w-full h-32 border container flex flex-wrap justify-between items-center mx-auto">
+        <li class="w"></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
