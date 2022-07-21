fb68375ed6f277e97fd3e7366718f1122c248ad8
R4rohitm <r4rohitm45@gmail.com>
2022-07-21 13:59:28 +0530

    new changes

--- Frontend/fraazo-app/src/index.js
+++ Frontend/fraazo-app/src/index.js
@@ -1,28 +1,15 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {BrowserRouter} from 'react-router-dom'
-
->>>>>>> 951dbe5642d95d825cade4b4b48b6ad8fe3f4124
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-  <React.StrictMode>
-    <BrowserRouter>
+    <React.StrictMode>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </React.StrictMode>
   </BrowserRouter>
 );
-
-
--- Frontend/fraazo-app/src/routes/Mainroutes.jsx
+++ Frontend/fraazo-app/src/routes/Mainroutes.jsx
@@ -3,14 +3,15 @@ import { Route, Routes } from "react-router-dom";
 import Menu from "../components/Product/Menu";
 import Login from "../components/user/Login";
 import Register from "../components/user/Register";
-
+import Navbar from "../components/Navbar/Navbar";
 const MainRoutes = () => {
   return (
     <div>
       <Routes>
+        <Route path="/navbar" element={<Navbar />}></Route>
         <Route path="/register" element={<Register />}></Route>
         <Route path="/login" element={<Login />}></Route>
-        <Route path="/product" element={< Menu/>}></Route>
+        <Route path="/product" element={<Menu />}></Route>
       </Routes>
     </div>
   );
