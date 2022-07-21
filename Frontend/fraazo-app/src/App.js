2d4f05807dc1aa7734f9a358b5bf60448c89a882
R4rohitm <r4rohitm45@gmail.com>
2022-07-21 14:02:40 +0530

    main

--- Frontend/fraazo-app/src/App.js
+++ Frontend/fraazo-app/src/App.js
@@ -1,24 +1,11 @@
-<<<<<<< HEAD
-import logo from "./logo.svg";
 import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
+import MainRoutes from "./routes/Mainroutes.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
+    <div>
+      <MainRoutes />
     </div>
-=======
-
-import './App.css';
-import MainRoutes from "./routes/Mainroutes.jsx"
-
-function App() {
-  return (
-   <div>
-<MainRoutes/>
-   </div>
->>>>>>> 951dbe5642d95d825cade4b4b48b6ad8fe3f4124
   );
 }
 
--- Frontend/fraazo-app/src/routes/Mainroutes.jsx
+++ Frontend/fraazo-app/src/routes/Mainroutes.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
 import Menu from "../components/Product/Menu";
 import Login from "../components/user/Login";
 import Register from "../components/user/Register";
@@ -8,9 +9,10 @@ const MainRoutes = () => {
   return (
     <div>
       <Routes>
+        <Route path="/navbar" element={<Navbar />}></Route>
         <Route path="/register" element={<Register />}></Route>
         <Route path="/login" element={<Login />}></Route>
-        <Route path="/product" element={< Menu/>}></Route>
+        <Route path="/product" element={<Menu />}></Route>
       </Routes>
     </div>
   );
