748d8de7c3b85048576060f5ddd9ceea67f74be4
Nameh-Dhiman <namehdhiman@gmail.com>
2022-07-22 09:58:02 +0530

    Model Changes

--- Backend/Models/ItemsModel.js
+++ Backend/Models/ItemsModel.js
@@ -1,17 +1,22 @@
 const {Schema, model, models} = require('mongoose');
 
 const ItemsSchema = new Schema({
-  name: { type: String, required: true },
-  qty: { type: String, required: true },
+  name: { type: String, required: [true, "Please enter your name"] },
+  qty: { type: String, required: [[true, "Please enter product selling quantity"]] },
   image: {
     type: String,
     default:
       "https://webasset.fraazo.com/production/b70a67f4e825e3d388ac4466952c20a8.svg",
-    required: true,
   },
-  price: { type: String, required: true },
-  category: { type: String, required: true },
-  subcategory: { type: String, required: true },
+  price: { type: String, required: [true, "Please enter a price"] },
+  category: {
+    type: String,
+    required: [true, "Please enter the product category"],
+  },
+  subcategory: {
+    type: String,
+    required: [true, "Please enter the product subcategory"],
+  },
 });
 
 const ItemsModel = new model("item", ItemsSchema);
--- Backend/routes/ItemsRoutes.js
+++ Backend/routes/ItemsRoutes.js
@@ -8,12 +8,26 @@ itemsRoutes.get("/", async (req, res) => {
   return res.json(items);
 });
 
+itemsRoutes.get("/:id", async (req, res) => {
+  const {id} = req.params;
+  const items = await ItemsModel.findOne({_id:id});
+  return res.json(items);
+});
+
 itemsRoutes.get("/search", async (req, res) => {
   const query = req.query.q;
   const items = await ItemsModel.find({
     name: { $regex: new RegExp(`${query}*`), $options: "gi" },
   });
   return res.json(items);
+});
+
+itemsRoutes.post("/", async (req, res) => {
+  const items = await ItemsModel({...req.body});
+  items.save((err, items) => {
+    if(err) return res.status(500).send(err);
+    else return res.status(201).json(items);
+  });
 });
 
 module.exports = itemsRoutes;
