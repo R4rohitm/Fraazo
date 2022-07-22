76c9e26c11b698de7a1b30e3a71b47c263c7aecd
Nameh-Dhiman <namehdhiman@gmail.com>
2022-07-22 16:07:19 +0530

    Models Updated

--- Backend/Controllers/UsersController.js
+++ Backend/Controllers/UsersController.js
@@ -1,5 +1,21 @@
 const UsersModel = require("../Models/UsersModel");
 
+function generateString(length) {
+  const characters =
+       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let result = " ";
+  
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  return `${result}-${random(100, 1000000000)}`;
+}
+
 const getUsers = async(req,res) => {
      const users = await UsersModel.find({});
      return res.json(users);
@@ -11,12 +27,27 @@ const getUserById = async(req,res) => {
      return res.json(user);
 }
 
-const createNewUser = async(req,res) => {
-    const user = await UsersModel({ ...req.body });
-    user.save((err, user) => {
-      if (err) return res.status(500).send(err);
-      else return res.status(201).json(user);
-    });
+const updateUserById = async(req,res) => {
+     const { id } = req.params;
+     const {title, firstname, lastname, email} = req.body;
+     const user = await UsersModel.updateOne({ _id: id }, {$set: {title: title, firstname: firstname, lastname: lastname, email: email}});
+     if(user.modifiedCount === 1 && user.matchedCount === 1){
+      return res.status(200).send("User Details Updated");
+     }else return res.status(404).send("User Not Found");
 }
 
-module.exports = {getUsers, getUserById, createNewUser};
+const createNewUser = async(req,res) => {
+    const {mobile, email, name} = req.body;
+    const userExists = await UsersModel.findOne({mobile: mobile, email: email, name: name}); 
+    if(userExists){
+     return res.status(400).json({error: "User already exists"});
+    }else {
+     const user = await UsersModel({ ...req.body, coupon: generateString(2) });
+     user.save((err, user) => {
+       if (err) return res.status(500).send(err);
+       else return res.status(201).json(user);
+     });
+    }
+}
+
+module.exports = {getUsers, getUserById, updateUserById, createNewUser};
--- Backend/routes/UserRoutes.js
+++ Backend/routes/UserRoutes.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
-const {getUsers, getUserById, createNewUser} = require('../Controllers/UsersController');
+const {getUsers, getUserById, updateUserById, createNewUser} = require('../Controllers/UsersController');
 
 const userRoutes = Router();
 
 userRoutes.get("/", getUsers);
 
 userRoutes.get("/:id", getUserById);
+
+userRoutes.patch("/:id", updateUserById);
 
 userRoutes.post("/", createNewUser);
 
